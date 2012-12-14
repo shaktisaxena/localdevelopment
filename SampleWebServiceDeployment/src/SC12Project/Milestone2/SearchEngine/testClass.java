a040b9eb8533c70bb679ceb6fea7f8347fab1b01
shaktisaxena <shaktisaxena@192.168.1.10>
2012-12-14 01:20:33 +0100

    weather

---
+++ SampleWebServiceDeployment/src/SC12Project/Milestone2/SearchEngine/testClass.java
@@ -0,0 +1,21 @@
+package SC12Project.Milestone2.SearchEngine;
+
+import java.util.List;
+
+public class testClass implements WSSearchEngine  {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<ServiceInfo> search(String keywords) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
